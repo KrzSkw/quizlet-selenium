5b77ea4239bbddfed928e3e16fd49bc4237bd15a
Krzysztof Skwiecinski <krzysztof.skwiecinski@elwas.pl>
2021-01-19 15:13:00 +0100

    Added code for accepting cookies

--- quizlet-selenium.py
+++ quizlet-selenium.py
@@ -3,6 +3,7 @@ from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver import ActionChains
 import time
 PATH = "C:\\Program Files (x86)\\chromedriver.exe"
 
@@ -10,3 +11,25 @@ driver = webdriver.Chrome(PATH)
 
 driver.get("https://quizlet.com/create-set")
 
+
+try:
+    button = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.ID, "onetrust-accept-btn-handler")))
+    time.sleep(2)
+    button.click()
+    print("button found")
+
+except:
+    print("button NOT found")
+    time.sleep(10)
+    driver.quit() #closes chrome
+
+# searchForTitle = driver.find_element_by_class_name("AutoExpandTextarea-textarea")
+# searchForTitle.send_keys("test quizlet title")
+# searchForTitle.send_keys(Keys.TAB)
+# searchForTitle.send_keys(Keys.TAB)
+
+
+
+
+time.sleep(10)
+driver.quit() #closes chrome
